ee92ef2fb37588a213766a8cd7acdaa75e222c7d
uzumaki-ak <anikydv38>
2026-05-02 00:57:13 +0530

    Shortened exp4 code

--- exp4_comment.c
+++ exp4_comment.c
@@ -2,20 +2,18 @@
 #include <string.h>
 #include <ctype.h>
 
-const char*check(const char*line){
-    while(*line && isspace((unsigned char)*line)) line++;
-    int n=(int)strlen(line); while(n>0 && isspace((unsigned char)line[n-1])) n--;
-    if(n>=2 && line[0]=='/' && line[1]=='/') return "Single-line //";
-    if(n>=1 && line[0]=='#') return "Single-line #";
-    if(n>=4 && line[0]=='/' && line[1]=='*' && line[n-2]=='*' && line[n-1]=='/') return "Single-line /* */";
-    if(n>=2 && line[0]=='/' && line[1]=='*') return "Start block";
-    if(n>=2 && line[n-2]=='*' && line[n-1]=='/') return "End block";
-    return "Not a comment";
+const char* c(const char* l) {
+    while(isspace(*l)) l++;
+    int n=strlen(l); while(n && isspace(l[n-1])) n--;
+    if(n>1 && l[0]=='/' && l[1]=='/') return "Single-line //";
+    if(n && l[0]=='#') return "Single-line #";
+    if(n>3 && l[0]=='/' && l[1]=='*' && l[n-2]=='*' && l[n-1]=='/') return "Single-line /* */";
+    return (n>1 && l[0]=='/' && l[1]=='*') ? "Start block" : (n>1 && l[n-2]=='*' && l[n-1]=='/') ? "End block" : "Not a comment";
 }
 
-int main(){
-    const char*L[]={"// Naruto comment","/* Supra */","/* Start"," End */","# python","int rx7=5;","goku=goku+1;","   // indented"};
+int main() {
+    const char* L[]={"// Naruto comment","/* Supra */","/* Start"," End */","# python","int rx7=5;","goku=goku+1;","   // indented"};
     printf("%-28s %s\n","Line","Result");
-    for(int i=0;i<8;i++) printf("%-28s %s\n",L[i],check(L[i]));
+    for(int i=0; i<8; i++) printf("%-28s %s\n", L[i], c(L[i]));
     return 0;
 }
